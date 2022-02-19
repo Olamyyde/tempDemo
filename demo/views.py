cc70d639bf0028249953a955a473548e1fdc54df
olamide <myyde8170@gmail.com>
2022-02-19 23:15:07 +0100

    added template tags

--- demo/views.py
+++ demo/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def render_template(request):
-    return render(request, 'tempApp/first.html')
+    my_dict = {"name": "Ok"}
+    return render(request, 'tempApp/first.html', context=my_dict)
 
--- tempDemo/urls.py
+++ tempDemo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/', views.render_template),
 ]
