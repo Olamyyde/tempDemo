67ff1ac3aeaca754402f80039cd643adcc6d788e
olamide <myyde8170@gmail.com>
2022-02-20 09:24:25 +0100

    added a method to render employee details

--- demo/views.py
+++ demo/views.py
@@ -5,3 +5,9 @@ def render_template(request):
     my_dict = {"name": "Ok"}
     return render(request, 'tempApp/first.html', context=my_dict)
 
+
+def render_employee(request):
+    my_dict = {"id": 123, "name": "Jon", "salary":10000}
+    return render(request, 'tempApp/employeeTemplate.html', context=my_dict)
+
+
